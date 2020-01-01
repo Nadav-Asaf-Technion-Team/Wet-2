351b6ec4d0253f9e4a9fc1265c089d189c3216af
Asaf Bialystok <asaf.b@campus.technion.ac.il>
2020-01-01 14:22:44 +0200

    commentng out irrelevant parts

--- main.cpp
+++ main.cpp
@@ -1,15 +1,15 @@
-#include "UnionFind.h"
-
-int main() {
-	UnionFind<int>* UN;
-	UN = new UnionFind<int>(10);
-	UN->print();
-	UN->Union(1, 2);
-	UN->Union(3, 4);
-	UN->Union(3, 2);
-	UN->Union(6, 9);
-	UN->Union(9, 5);
-	UN->Union(1, 9);
-	UN->Union(7, 8);
-	UN->print();
-}
+//#include "UnionFind.h"
+//
+//int main() {
+//	UnionFind<int>* UN;
+//	UN = new UnionFind<int>(10);
+//	UN->print();
+//	UN->Union(1, 2);
+//	UN->Union(3, 4);
+//	UN->Union(3, 2);
+//	UN->Union(6, 9);
+//	UN->Union(9, 5);
+//	UN->Union(1, 9);
+//	UN->Union(7, 8);
+//	UN->print();
+//}
